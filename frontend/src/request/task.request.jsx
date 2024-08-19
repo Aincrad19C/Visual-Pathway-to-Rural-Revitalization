815aa0d48c99fcbb165bed93512eab7952a64220
TwinklerG <326598880@qq.com>
2024-08-19 21:43:17 +0800

    front can contact back

--- frontend/src/request/task.request.jsx
+++ frontend/src/request/task.request.jsx
@@ -2,11 +2,11 @@ import * as axios from 'axios';
 
 const client = axios.default;
 
-client.get('http://127.0.0.1:7001').then((response) => {
+client.get('backend:7001').then((response) => {
     console.log(response);
 })
 
-client.post('http://127.0.0.1:7001/task/create', {
+client.post('backend:7001/task/create', {
     name:"界面",
     description:"设计界面"
 },
